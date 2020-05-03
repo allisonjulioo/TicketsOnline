ae14a6de2e969b831bdd99826d81e8053fc419dc
Allison Nunes <allison.julio@hotmail.com>
2020-05-02 23:46:28 -0300

    Add behavior buttons

--- src/pages/Home/components/Banner/index.jsx
+++ src/pages/Home/components/Banner/index.jsx
@@ -5,6 +5,18 @@ import Banner from "./../../../../assets/banner.png";
 export default () => {
   return (
     <div id="banner">
+      <div className="title">
+        <h1>Blood Shot 2020</h1>
+        <p>
+          Baseado no best-seller de banda desenhada, Vin Diesel protagoniza
+          "Bloodshot" na pele de Ray Garrison, um soldado recentemente morto em
+          combate e ressuscitado como o super-humano Bloodshot da empresa RST.
+        </p>
+        <div className="row">
+        <button>ingresso</button>
+        <button>trailer</button>
+        </div>
+      </div>
       <img src={Banner} alt="" />
     </div>
   );
