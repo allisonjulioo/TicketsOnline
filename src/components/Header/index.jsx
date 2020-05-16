ab7d9387f8598dd6484f8a819b4cf07442e2092d
SIGGA\allison.nunes <allison.nunes@sigga.com>
2020-05-16 18:14:30 -0300

    Branding fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "@/assets/logo.png";
+import logoLight from "@/assets/logo-light.png";
 import UserDropdown from "./components/Dropdown"; 
 import "./styles.scss";
 
