800382782f5778be5e07ab9d96df2bae300026f6
Allison Nunes <allison.nunes@sigga.com>
2020-06-14 00:18:07 -0300

    Add scss admin mode

--- src/pages/Admin/Dashboard/index.jsx
+++ src/pages/Admin/Dashboard/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Button from "@/components/Button";
 import { Link } from "react-router-dom";
 import "./styles.scss";
 
 export default (props) => {
   return (
-    <div id="dashboard" style={{ marginTop: "110px" }}>
+    <div id="dashboard" className="container" style={{ padding: "2em" }}>
       <h3>Dashboard</h3>
-      <Link to="/">Voltar</Link>
+      <Link to="/">
+        <Button>Voltar</Button>
+      </Link>
+      <Link to="/admin/movie/new">
+        <Button>Cadastrar filme</Button>
+      </Link>
     </div>
   );
 };
